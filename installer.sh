3ff1d3983344e5c72e2a48b42839ed7a2e2d00c4
cidazo <66745737+cidazo@users.noreply.github.com>
2021-11-16 17:01:21 +0100

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+curr_path=$(pwd)
+
+echo "Free instalation of insomnia on elementary os 6"
+echo "Repo is: https://github.com/manexim/insomnia.git"
+
+sudo apt install -y libgranite-dev libgtk-3-dev libhandy-1-dev meson valac
+
+cd ~/Desktop
+git clone https://github.com/manexim/insomnia.git
+cd insomnia
+meson build --prefix=/usr
+cd build
+ninja
+sudo ninja install
+
+cd $curr_path
+
+chmod 600 insomnia.desktop
+
+mv insomnia.desktop ~/.local/share/applications
+
+echo "Installed as an application"
+echo "To uninstall execute: sudo ninja uninstall"
+echo
